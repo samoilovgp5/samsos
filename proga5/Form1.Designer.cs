c4037ad52ca96c4c5e55e84b69ec1c6d1bd758e6
Павел Соколов <pasha040302@yandex.ru>
2022-03-26 10:44:41 +0300

    иван

--- proga5/Form1.Designer.cs
+++ proga5/Form1.Designer.cs
@@ -35,39 +35,43 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(71, 12);
+            this.button1.Location = new System.Drawing.Point(324, 240);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(151, 115);
+            this.button1.Size = new System.Drawing.Size(201, 142);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "иван хватит казтино";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(330, 22);
+            this.button2.Location = new System.Drawing.Point(440, 27);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(151, 115);
+            this.button2.Size = new System.Drawing.Size(201, 142);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(513, 35);
+            this.button3.Location = new System.Drawing.Point(718, 81);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(151, 115);
+            this.button3.Size = new System.Drawing.Size(201, 142);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
