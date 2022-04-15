b778172583cfc177143fb9cec251c5a256384b0a
Pasha <pjoi6547@gmail.com>
2022-04-15 20:26:02 +0300

    эээ

--- proga5/Form1.Designer.cs
+++ proga5/Form1.Designer.cs
@@ -53,6 +53,7 @@
             this.button3.TabIndex = 2;
             this.button3.Text = "Слава";
             this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
--- proga5/Form1.cs
+++ proga5/Form1.cs
@@ -16,5 +16,10 @@ namespace proga5
         {
             InitializeComponent();
         }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
