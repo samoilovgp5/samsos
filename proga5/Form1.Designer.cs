9d91737fc19072d2d74dc38fff7123d28310ed44
Pasha <pjoi6547@gmail.com>
2022-04-15 21:15:26 +0300

    qwerty

--- proga5/Form1.Designer.cs
+++ proga5/Form1.Designer.cs
@@ -28,64 +28,51 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            this.sanekBTN = new System.Windows.Forms.Button();
+            this.sashinTimer = new System.Windows.Forms.Timer(this.components);
+            this.checqwewqewq = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
-            // button2
+            // sanekBTN
             // 
-            this.button2.BackColor = System.Drawing.Color.Yellow;
-            this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(167, 183);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(10, 112);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Украинек";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.sanekBTN.Location = new System.Drawing.Point(109, 154);
+            this.sanekBTN.Name = "sanekBTN";
+            this.sanekBTN.Size = new System.Drawing.Size(215, 71);
+            this.sanekBTN.TabIndex = 0;
+            this.sanekBTN.Text = "uiqwerfwriuerwiuerfyiuesgyeriougdf";
+            this.sanekBTN.TextAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.sanekBTN.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // checqwewqewq
             // 
-            this.button3.BackColor = System.Drawing.Color.Blue;
-            this.button3.ForeColor = System.Drawing.Color.Black;
-            this.button3.Location = new System.Drawing.Point(12, 22);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(410, 111);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Слава";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Yellow;
-            this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(98, 301);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(523, 101);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "ПУТИН";
-            this.button1.UseVisualStyleBackColor = false;
+            this.checqwewqewq.AutoSize = true;
+            this.checqwewqewq.Location = new System.Drawing.Point(459, 174);
+            this.checqwewqewq.Name = "checqwewqewq";
+            this.checqwewqewq.Size = new System.Drawing.Size(80, 17);
+            this.checqwewqewq.TabIndex = 1;
+            this.checqwewqewq.Text = "checkBox1";
+            this.checqwewqewq.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.checqwewqewq);
+            this.Controls.Add(this.sanekBTN);
             this.Name = "Form1";
             this.Text = "ФОРМА БАНА ЖОПЫ";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button1;
+
+        private System.Windows.Forms.Button sanekBTN;
+        private System.Windows.Forms.Timer sashinTimer;
+        private System.Windows.Forms.CheckBox checqwewqewq;
     }
 }
 
