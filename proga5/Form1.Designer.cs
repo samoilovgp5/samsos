355164f2e6c55b84216a495af663f0dbf03af757
Alex a <rimpelkop12@inbox.ru>
2022-04-15 19:25:21 +0300

    Я КРИНЖАНУЛ

--- proga5/Form1.Designer.cs
+++ proga5/Form1.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
@@ -38,28 +39,39 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(151, 115);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "УРА ПУТИНУ";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(381, 181);
+            this.button2.Location = new System.Drawing.Point(271, 77);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(151, 115);
             this.button2.TabIndex = 1;
             this.button2.Text = "взорвать америку!!!!!!!!!!!!!!!!!";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 259);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(321, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "ХУЙ";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "ФОРМА ЖОПЫ";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -67,6 +79,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
