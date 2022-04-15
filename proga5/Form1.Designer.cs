f5b3b7cdf608226f58efa9afb3073e6db0208170
Pasha <pjoi6547@gmail.com>
2022-04-15 20:21:19 +0300

    иван лох

--- proga5/Form1.Designer.cs
+++ proga5/Form1.Designer.cs
@@ -28,41 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(151, 23);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(524, 115);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "ЗАБАНИТЬ ИВАНИКА";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
             // button2
             // 
-            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.BackColor = System.Drawing.Color.Yellow;
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(151, 265);
+            this.button2.Location = new System.Drawing.Point(132, 205);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(524, 115);
             this.button2.TabIndex = 1;
-            this.button2.Text = "ЗАБАНИТЬ ПАШКА";
+            this.button2.Text = "Украинек";
             this.button2.UseVisualStyleBackColor = false;
             // 
             // button3
             // 
             this.button3.BackColor = System.Drawing.Color.Blue;
             this.button3.ForeColor = System.Drawing.Color.White;
-            this.button3.Location = new System.Drawing.Point(151, 144);
+            this.button3.Location = new System.Drawing.Point(132, 84);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(524, 115);
             this.button3.TabIndex = 2;
-            this.button3.Text = "ЗАБАНИТЬ САНЬКА";
+            this.button3.Text = "Слава";
             this.button3.UseVisualStyleBackColor = false;
             // 
             // Form1
@@ -72,7 +61,6 @@
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "ФОРМА БАНА ЖОПЫ";
             this.ResumeLayout(false);
@@ -80,8 +68,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
     }
