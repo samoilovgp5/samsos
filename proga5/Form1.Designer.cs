fcbf843ba8496ae6cadbb8a0c9a54b3bf55849bd
Alex a <rimpelkop12@inbox.ru>
2022-04-15 20:56:08 +0300

    КРИНГАНУЛ ЖЕСТКО

--- proga5/Form1.Designer.cs
+++ proga5/Form1.Designer.cs
@@ -32,10 +32,8 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
             this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button2
@@ -86,18 +84,6 @@
             this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
-            // button5
-            // 
-            this.button5.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button5.ForeColor = System.Drawing.Color.Black;
-            this.button5.Location = new System.Drawing.Point(563, 325);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(523, 95);
-            this.button5.TabIndex = 8;
-            this.button5.Text = "AAAA";
-            this.button5.UseVisualStyleBackColor = false;
-            this.button5.Click += new System.EventHandler(this.button5_Click);
-            // 
             // button6
             // 
             this.button6.BackColor = System.Drawing.SystemColors.ButtonHighlight;
@@ -120,26 +106,13 @@
             this.button7.Text = "Слава";
             this.button7.UseVisualStyleBackColor = false;
             // 
-            // button8
-            // 
-            this.button8.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button8.ForeColor = System.Drawing.Color.Black;
-            this.button8.Location = new System.Drawing.Point(563, 124);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(523, 88);
-            this.button8.TabIndex = 5;
-            this.button8.Text = "ДЕДОК";
-            this.button8.UseVisualStyleBackColor = false;
-            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1148, 454);
-            this.Controls.Add(this.button5);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button7);
-            this.Controls.Add(this.button8);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.button3);
@@ -155,10 +128,8 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
         private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button8;
     }
 }
 
